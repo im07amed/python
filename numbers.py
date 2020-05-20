846b40119e6490f1e49ada94ca78246680254afb
im07amed <im07amed@users.noreply.github.com>
2020-05-20 04:23:58 -0400

    numbers

---
+++ numbers.py
@@ -0,0 +1,9 @@
+print(10 + int('2'))
+print(3 ** 10) #powerof
+
+num1 = 10
+num2 = 20
+num3 = num1+num2
+print(num3)
+num2 +=1
+print(num2)
---
+++ stings.py
@@ -0,0 +1,4 @@
+fname = 'M7md'
+lname = 'Mbark'
+fullname = 'M7md Mbark'
+print(fullname.replace('M7md','Ali'))
