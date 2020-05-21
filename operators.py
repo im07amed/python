53a5c52484552a157dc2345166b5d611bb027527
im07amed <im07amed@users.noreply.github.com>
2020-05-21 09:26:38 -0400

    operators

---
+++ operators.py
@@ -0,0 +1,17 @@
+num1 = 5
+num2 = 6
+print(num1 < num2)
+print(5 > 6)
+print(5 != 6)
+
+print(True and True)
+print(False and True)
+a,b = 1,2
+print(a)
+print(a == a and b != b)
+
+print(False or True)
+print(a > b or b < a)
+
+print(not True)
+print(not (a ==5))
