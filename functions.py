35cda52f5e6b41e4e0f83ae3e6d30e83de04a73e
im07amed <im07amed@users.noreply.github.com>
2020-05-25 14:04:54 -0400

    function

---
+++ functions.py
@@ -0,0 +1,14 @@
+def liner():
+    print('..............')
+
+def addition(num1, num2):
+    print(int(num1) + int(num2))
+
+n1 = input('Please enter the 1st No.: ')
+n2 = input('Please enter the 2nd No.: ')
+addition(n1,n2)
+
+print('M7md A7md')
+liner()
+print(22)
+liner()
