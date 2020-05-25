87113607299f23b41ccce24d9ae6c55b227ea712
im07amed <im07amed@users.noreply.github.com>
2020-05-25 13:56:55 -0400

    diction

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+subjects_with_no_student = {'Arabic':3, 'English':2}
+print(subjects_with_no_student)
+Students = {'Arabic':['Ali','M7md'], 'English':['Jack','John']}
+print(Students)
+Students['French'] = ['Salah']
+print(Students)
+Students.update({'Spanich': 'A7md'})
+print(Students)
